939ad8c25d96692d3d11ed49a656ec20b7d66f10
Itunu Fatoki <itunuworks@gmail.com>
2017-03-12 01:07:22 +0100

    Implemented the complete dataTypes function which passes all available tests

--- apps/andelabs.js
+++ apps/andelabs.js
@@ -2,12 +2,29 @@
 
 module.exports = {
 	dataTypes: function(argument){
-		if (typeof(argument) == 'number'){
-			return getNumberResult(argument);
+		switch (typeof (argument)){
+			case 'number':
+				return getNumberResult(argument); break;
+			case 'string':
+				return argument.length; break;
+			case 'object':
+				if (argument === null){
+					return 'no value'; break;
+				}
+				else{
+					return argument[2]; break;
+				}
+			case 'undefined':
+				return 'no value'; break;
+			case 'boolean':
+				return argument; break;
+			case 'function':
+				return argument(true); break;
 		}
 	}
 }
 
+//This function handles number arguments.
 function getNumberResult(argument){
 	if (argument != 100){
 		if (argument > 100){
