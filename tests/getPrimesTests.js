3391fea8b9c5d394758e7982670e5eb42bed187a
Itunu Fatoki <itunuworks@gmail.com>
2017-03-12 18:02:31 +0100

    Completed set up of 8 spec tests which all failed as expected.

--- tests/getPrimesTests.js
+++ tests/getPrimesTests.js
@@ -2,11 +2,51 @@
 
 var getPrimes = require('../apps/andelabs.js').getPrimes;
 
-describe('Function getPrimes ', function(){
+describe('Function getPrimes, ', function(){
 
-	describe('All cases, ', function(){
+	describe('in all cases, ', function(){
 		it('should return an array', function(){
 			expect(getPrimes(6)).toBeArray();
 		})
 	})
+
+	describe('when fed with invalid input data, ', function(){
+		describe("in this case, a string, 'itunu', ", function(){
+			it("should return []", function(){
+				expect(getPrimes('itunu')).toBe([]);
+			})
+		})
+	})
+
+	describe("when fed with a number less than 2", function(){
+		describe('in this case 0, ', function(){
+			it("should return []", function(){
+				expect(getPrimes(0)).toEqual([]);
+			})
+		})
+
+		describe('in this case -1, ', function(){
+			it("should return []", function(){
+				expect(getPrimes(-1)).toEqual([]);
+			})
+		})
+
+		describe("when fed with 1, ", function(){
+			it("should return []", function(){
+				expect(getPrimes(1)).toEqual([]);
+			})
+		})
+	})
+
+	it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53] for 55.", function(){
+		expect(getPrimes(55)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]);
+	});
+
+	it("should return [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41] for 42.", function(){
+		expect(getPrimes(42)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]);
+	});
+
+	it("should return [2, 3, 5, 7, 11, 13, 17, 19] for 20.", function(){
+		expect(getPrimes(20)).toEqual([2, 3, 5, 7, 11, 13, 17, 19]);
+	});
 })
